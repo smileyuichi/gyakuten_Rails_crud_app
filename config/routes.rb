4c0d23eb5108660af66b0d95c8854cb79bc7a693
YuichiTanaka <smile.yuichi.1013@gmail.com>
2020-06-10 23:53:45 +0900

    resources crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get "/users" => "users#index"
-  get "/users/new" => "users#new"
-  post "/users" => "users#create"
-  get "/users/:id/edit" => "users#edit"
-  patch "/users/:id" => "users#update"
-  delete "/users/:id" => "users#destroy"
+#   get "/users" => "users#index"
+#   get "/users/new" => "users#new"
+#   post "/users" => "users#create"
+#   get "/users/:id/edit" => "users#edit"
+#   patch "/users/:id" => "users#update"
+#   delete "/users/:id" => "users#destroy"
+
+  resources :users
 end
