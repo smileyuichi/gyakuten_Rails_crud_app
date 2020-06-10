b6d4c2de08950bf66476cd941030c090b2b6fcf6
YuichiTanaka <smile.yuichi.1013@gmail.com>
2020-06-10 23:04:48 +0900

    no resources clud

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,32 @@
+class UsersController < ApplicationController
+    def index
+        @users = User.all
+    end
+    
+    def new
+        @user = User.new
+    end
+
+    def create
+        User.create(user_params)
+    end
+
+    def edit
+        @user = User.find_by(id: params[:id])
+    end
+    
+    def update
+        user = User.find_by(id: params[:id])
+        user.update(user_params)
+    end 
+
+    def destroy
+        user = User.find_by(id: params[:id])
+        user.delete
+    end
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :age)
+    end
+end
